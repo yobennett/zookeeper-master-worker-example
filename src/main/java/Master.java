5382d15f6c0c6fdc6f5cbf3c774170d828f095e2
Bennett <bennett@yobennett.com>
2015-05-16 13:13:34 -0700

    add skeleton Master watcher
    Implement the Watcher interface to demonstrate a client connecting to
    ZooKeeper. We simply print out WatchedEvent objects as they are processed.

---
+++ src/main/java/Master.java
@@ -0,0 +1,32 @@
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+public class Master implements Watcher {
+
+    ZooKeeper zk;
+    String hostPort;
+
+    Master(String hostPort) {
+        this.hostPort = hostPort;
+    }
+
+    void startZk() throws IOException {
+        zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    public void process(WatchedEvent e) {
+        System.out.println(e);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Master m = new Master(args[0]);
+        m.startZk();
+
+        // wait
+        Thread.sleep(60000);
+    }
+
+}
